7a4c54408ee4db7311ccfc82b511a94462837c9b
IaOksana <iashchuk.oksana@gmail.com>
2025-02-17 21:17:31 +0400

    redirection processed

--- script.sh
+++ script.sh
@@ -7,8 +7,12 @@ for j in "${websites[@]}"; do
   res=$(curl -s -o /dev/nul -w "%{http_code}" $j)
   if [ $res -eq 200 ]
   then echo "Index: $j, status: $res - is UP" | tee -a "$log_file"
+  elif [ $res -eq 301 ] || [ $res -eq 302 ]
+  then
+  redirected_url=$(curl -Ls -o /dev/nul -w "%{url_effective}" $j)
+  echo "Index: $j, status: $res - is REDIRECTED to $redirected_url" | tee -a "$log_file"
   else
   echo "Index: $j, status: $res  - is DOWN" | tee -a "$log_file"
   fi
 done
-echo "All the results are in $logfile "
+echo "All the results are in $log_file "
